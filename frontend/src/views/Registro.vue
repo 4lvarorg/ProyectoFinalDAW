9ece4d4bd6db332a58c98e99d406d138e7ffc2ba
“[Alvaro <[alvarorg@pm.me]>
2023-06-20 18:35:35 +0200

    fix

--- frontend/src/views/Registro.vue
+++ frontend/src/views/Registro.vue
@@ -1,24 +1,64 @@
 <template>
   <div class="registro">
     <h1>Registro</h1>
-    <form>
+    <form @submit.prevent="register">
+      <div class="form-group">
+        <label for="type">Tipo de Registro</label>
+        <select v-model="type" id="type" class="form-control" required>
+          <option value="usuario">Usuario</option>
+          <option value="psicologo">Psicólogo</option>
+        </select>
+      </div>
       <div class="form-group">
         <label for="email">Correo Electrónico</label>
-        <input type="email" class="form-control" id="email" required>
+        <input type="email" v-model="email" class="form-control" id="email" required>
       </div>
       <div class="form-group">
         <label for="password">Contraseña</label>
-        <input type="password" class="form-control" id="password" required>
+        <input type="password" v-model="password" class="form-control" id="password" required>
       </div>
+      <!-- Add additional fields as needed -->
       <button type="submit" class="btn btn-primary">Registrar</button>
     </form>
   </div>
 </template>
 
 <script>
+import UsuarioService from '../services/UsuarioService';
+import PsicologoService from '../services/PsicologoService';
+
 export default {
   name: 'Registro',
-}
+  data() {
+    return {
+      type: 'usuario',
+      email: '',
+      password: '',
+      // Add additional fields as needed
+    };
+  },
+  methods: {
+    async register() {
+      const data = {
+        email: this.email,
+        password: this.password,
+        // Add additional fields as needed
+      };
+
+      try {
+        if (this.type === 'usuario') {
+          await UsuarioService.insertarUsuario(data);
+        } else {
+          await PsicologoService.insertarPsicologo(data);
+        }
+
+        alert('Registro exitoso!');
+      } catch (error) {
+        alert('Hubo un error en el registro. Por favor, vuelve a introducir los datos.');
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
