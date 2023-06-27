3755ae0acf6dd0141ae2ae91f4d8650bd97e9b10
“[Alvaro <[alvarorg@pm.me]>
2023-06-27 23:38:22 +0200

    fix

--- ScriptSQl/ScriptSQL.sql
+++ ScriptSQl/ScriptSQL.sql
@@ -34,7 +34,7 @@ CREATE TABLE `cita` (
   KEY `FKb3l8n6flyfg5uxsvimq1idnt8` (`usuario_id`),
   CONSTRAINT `FKb2b9jna1rju8mphfebalfkfs8` FOREIGN KEY (`psicologo_id`) REFERENCES `psicologo` (`id`),
   CONSTRAINT `FKb3l8n6flyfg5uxsvimq1idnt8` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,7 +43,7 @@ CREATE TABLE `cita` (
 
 LOCK TABLES `cita` WRITE;
 /*!40000 ALTER TABLE `cita` DISABLE KEYS */;
-INSERT INTO `cita` (`id`, `fecha_reservada`, `hora_reservada`, `precio_final`, `psicologo_id`, `usuario_id`) VALUES (7,'2023-06-19','21:00:00',12,1,2),(10,'2023-06-23','21:00:00',10,1,2),(11,'2023-06-25','15:00:00',10,1,2),(12,'2023-06-28','21:00:00',10,1,2),(16,'2023-06-30','21:00:00',10,1,2),(17,'2023-06-30','21:00:00',8,1,2),(18,'2023-06-30','21:00:00',8,1,2),(19,'2023-06-30','21:00:00',8,1,2),(20,'2023-06-30','21:00:00',8,1,2),(21,'2023-06-30','21:00:00',8,1,2),(22,'2023-06-30','21:00:00',0,1,2),(23,'2023-08-31','20:00:00',0,1,2),(24,'2023-12-12','21:00:00',0,1,2),(25,'2023-11-23','17:00:00',15,8,2),(26,'2023-09-12','13:00:00',0,1,2),(27,'2023-08-15','17:00:00',0,1,2),(28,'2023-07-25','22:00:00',0,1,2),(29,'2023-05-31','23:00:00',10,8,2),(30,'2023-05-31','15:00:00',0,1,2);
+INSERT INTO `cita` (`id`, `fecha_reservada`, `hora_reservada`, `precio_final`, `psicologo_id`, `usuario_id`) VALUES (34,'2022-05-31','21:00:00',44,1,27),(35,'2025-02-02','22:00:00',2,12,28),(36,'2023-01-01','22:00:00',2,12,28),(37,'2023-04-04','22:00:00',2,12,28),(38,'2025-06-12','22:00:00',2,12,28),(39,'2023-09-01','22:00:00',2,12,28),(40,'2026-02-08','22:00:00',1.6,12,28),(41,'2023-06-23','21:00:00',44,1,28),(42,'2023-08-15','21:00:00',10,8,28),(43,'2023-04-23','21:00:00',44,1,28),(44,'2023-09-01','21:00:00',44,1,28),(45,'2023-08-30','22:00:00',1.6,12,28),(46,'2030-06-11','21:00:00',44,1,28),(47,'2034-05-31','21:00:00',44,1,28),(48,'2023-09-12','21:00:00',10,8,28);
 /*!40000 ALTER TABLE `cita` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -134,7 +134,7 @@ CREATE TABLE `usuario` (
   UNIQUE KEY `UK_lyn4jrsa2ou2meyuarytj8tcc` (`telefono`),
   KEY `FK3qieopaxyoowdge29v1m51wr9` (`role_id`),
   CONSTRAINT `FK3qieopaxyoowdge29v1m51wr9` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -143,7 +143,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` (`id`, `apellido`, `codigo_postal`, `direccion`, `email`, `nombre`, `password`, `telefono`, `role_id`) VALUES (1,'admin',111111,'admin','admin@admin.com','admin','$2a$12$CrDniOHP5crT/vlTXwVaNeMxZyRQPwmZ70Z3IrgET3yT7buAZkHwi',611557700,1),(2,'reb',28005,'madrid','reb@reb.com','reb','$2a$12$60wx7NdhhrGLzgGrMKkwWe2slMzEMIdcgQRBqhOu8dzzpLSqyYesO',622552255,2);
+INSERT INTO `usuario` (`id`, `apellido`, `codigo_postal`, `direccion`, `email`, `nombre`, `password`, `telefono`, `role_id`) VALUES (1,'admin',111111,'admin','admin@admin.com','admin','$2a$12$CrDniOHP5crT/vlTXwVaNeMxZyRQPwmZ70Z3IrgET3yT7buAZkHwi',611557700,1),(27,'AsomaLaCabeza',56000,'cabezon','antonio@anton.com','Anton',NULL,686989699,2),(28,'hola',1234,'cucu','hola@hola.com','hola',NULL,923252525,2);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -156,4 +156,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-25 21:05:43
+-- Dump completed on 2023-06-27 23:35:35
